6fdb6438432c24d2e6470ed8fd6e006af01a0c62
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2024-12-26 02:21:17 +0530

    Result done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <div className="flex items-center justify-between gap-4 py-3 mt-20">
       <img width={150} src="/logo.png" alt="logo" />
-      <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">Copyright © PixScribe. All rights reserved.</p>
+      <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
+        Copyright © PixScribe. All rights reserved.
+      </p>
       <div className="flex gap-2.5">
         <img width={35} src={assets.facebook_icon} alt="fb_icon" />
         <img width={35} src={assets.twitter_icon} alt="fb_icon" />
