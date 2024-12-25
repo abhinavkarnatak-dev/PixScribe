b427fae5564585e62c8486bd235652cf9b22f8e0
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2024-12-26 03:28:46 +0530

    Login Signup pages added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -7,8 +7,6 @@ import sample_img_2 from './sample_img_2.png'
 import step_icon_1 from './step_icon_1.svg'
 import step_icon_2 from './step_icon_2.svg'
 import step_icon_3 from './step_icon_3.svg'
-import email_icon from './email_icon.svg'
-import lock_icon from './lock_icon.svg'
 import cross_icon from './cross_icon.svg'
 import star_group from './star_group.png'
 import credit_star from './credit_star.svg'
@@ -21,8 +19,6 @@ export const assets = {
     star_icon,
     sample_img_1,
     sample_img_2,
-    email_icon,
-    lock_icon,
     cross_icon,
     star_group,
     credit_star,
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,8 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const { user } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-between py-4">
       <Link to="/">
@@ -42,7 +43,10 @@ const Navbar = () => {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button
+              onClick={() => setShowLogin(true)}
+              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full"
+            >
               Login
             </button>
           </div>
